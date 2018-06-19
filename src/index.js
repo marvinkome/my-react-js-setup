7f5db1fb176e17be84e8f00d5f627b2338ba9475
marvinkome <marvinkome@gmail.com>
2018-06-19 01:00:45 +0100

    Add tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // Webpack entry file
 import React from 'react';
 import { render } from 'react-dom';
-
-const App = () => <p>hello</p>
+import { App } from './app';
 
 render(<App/>, document.getElementById('react-entry'))
---
+++ src/tests/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { App } from '../app';
+import { wrap } from 'module';
+
+describe('App tests', () => {
+    it('renders correctly', () => {
+        const app = shallow(<App/>)
+        expect(app).toMatchSnapshot()
+    })
+})
